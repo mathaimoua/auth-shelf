943f16a025b1c405d60474192a6c451daed8f851
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-08-31 15:43:48 -0500

    started delete

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 function ShelfPage() {
+
+  const deleteItem(item.id)
+
   return (
     <div className="container">
       <h2>Shelf</h2>
+      {items.map(item => {
+        return (
+          <p>{item}</p>
+          {deleteItem(item.id)}
+        )
+      })}
       <p>All of the available items can be seen here.</p>
     </div>
   );
