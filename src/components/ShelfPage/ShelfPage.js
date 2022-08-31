c225a23c32ac357f7277b08498727f90a2dd6ac7
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-08-31 15:52:26 -0500

    delete

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { useReducer } from 'react';
 
 function ShelfPage() {
 
-  const deleteItem(item.id)
+  const deleteItem = (itemID) => {
+    // might have to loop but i dont think so?
+    if (user.id === itemID) {
+      <button onClick={() => handleDelete()}>delete</button>
+    }
+  }
 
   return (
     <div className="container">
       <h2>Shelf</h2>
       {items.map(item => {
         return (
-          <p>{item}</p>
-          {deleteItem(item.id)}
+          <div>
+            <p>{item}</p>
+            {deleteItem(item.user_id)}
+          </div>
         )
       })}
       <p>All of the available items can be seen here.</p>
