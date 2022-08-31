46971134e5647657983f1383768b8a3196249732
Matt Moua <matmoua@gmail.com>
2022-08-31 16:38:19 -0500

    set up add item form and post method to db

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import AddForm from '../AddForm/AddForm';
+import {useSelector} from 'react-redux'
 
 function ShelfPage() {
+
+  const store = useSelector(store => store.user)
+
   return (
     <div className="container">
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
+      {store.id && <AddForm />}
     </div>
   );
 }
