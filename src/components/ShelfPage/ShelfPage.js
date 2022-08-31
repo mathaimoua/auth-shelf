ebbe988ae44a5d024321cc8cfd63c948ddb65333
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-08-31 16:55:21 -0500

    displayed to dom

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import { useReducer } from 'react';
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useReducer } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 function ShelfPage() {
 
@@ -13,26 +11,31 @@ function ShelfPage() {
     })
   }, [])
 
-  const deleteItem = (itemID) => {
-    // might have to loop but i dont think so?
-    if (user.id === itemID) {
-      <button onClick={() => handleDelete()}>delete</button>
-    }
-  }
+  const items = useSelector(store => store.items)
+
+  // const deleteItem = (itemID) => {
+  //   // might have to loop but i dont think so?
+  //   if (user.id === itemID) {
+  //     <button onClick={() => handleDelete()}>delete</button>
+  //   }
+  // }
 
   return (
+    <>
     <div className="container">
       <h2>Shelf</h2>
       {items.map(item => {
         return (
           <div>
-            <p>{item}</p>
-            {deleteItem(item.user_id)}
+            <p key={item.id}>{item.description}</p>
+            <img src={item.image_url} />
+            {/* {deleteItem(item.user_id)} */}
           </div>
         )
       })}
       <p>All of the available items can be seen here.</p>
     </div>
+    </>
   );
 }
 
